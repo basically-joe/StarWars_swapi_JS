aa8f64d99903aeaa7cf9cf9781c09f92daf3daf0
Joe McColl <joe4kool@gmail.com>
2019-01-18 16:30:11 +0000

    added get request to model

--- src/models/StarWarsCharacters.js
+++ src/models/StarWarsCharacters.js
@@ -2,7 +2,20 @@ const RequestHelper = require('../helpers/request_helper.js');
 const PubSub = require('../helpers/pub_sub.js');
 
 const StarWarsCharacters = function () {
-
+  this.data = []
 }
+
+StarWarsCharacters.prototype.getData = function () {
+  const url = `https://swapi.co/api/people/`;
+  const request = new RequestHelper(url);
+
+  const myPromise = request.get();
+  myPromise.then((data) => {
+    PubSub.publish('StarWarsCharacters:character-data-loaded', this.data);
+  })
+  .catch((err) =>{
+    console.error(err);
+  });
+};
 
 module.exports = StarWarsCharacters;
