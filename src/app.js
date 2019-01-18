2a67dfaa4838fd50b98fcfcd3cbbc143cf91cbc5
Joe McColl <joe4kool@gmail.com>
2019-01-18 21:18:19 +0000

    subscribed to data from model and data picked up in container view, logged output to console successfully

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 const StarWarsCharacters = require('./models/StarWarsCharacters.js');
+const ContainerView = require('./views/container_view.js');
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript Loaded');
 
   const starWarsCharacters = new StarWarsCharacters;
   starWarsCharacters.getData();
+
+  const container = document.querySelector('#star-wars-characters-container')
+  const containerView = new ContainerView(container);
+  containerView.bindEvents();
 
 });
--- src/views/container_view.js
+++ src/views/container_view.js
@@ -1,8 +1,15 @@
 const PubSub = require('../helpers/pub_sub.js');
 
 
-const ContainerView = function () {
+const ContainerView = function (container) {
   this.container = container;
+};
+
+ContainerView.prototype.bindEvents = function () {
+  PubSub.subscribe("StarWarsCharacters:characters-data-loaded", (event) => {
+    this.characters = event.detail;
+    console.log(`I'm logging the container: ${this.characters}`);
+  })
 };
 
 
